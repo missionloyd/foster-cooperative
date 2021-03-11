0d8c2d414ceed801bb1a84f89dcaa99e5c5195ad
Ariadne Dimarogona <adimarogona@gmail.com>
2021-03-11 12:53:43 -0800

    Update post.js
    added a few fields talked about adding last meeting. also added altimagetext field. notes: i have no idea how to add image field with image path

--- backend/models/post.js
+++ backend/models/post.js
@@ -5,7 +5,14 @@ const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
   date: { type: String, required: true },
-  author: [{ type: mongoose.Types.ObjectId, required: true, ref: 'user' }]
+  content: { type: String, required: true, minlength=1, maxlength=2000},
+  like: {type:String, required: true},
+  comment: {type: String, required=true, minlength = 1, maxlength = 1000},
+  author: { type: mongoose.Types.ObjectId, required: true, ref: 'user' },
+  //add image path ask how
+  //for accessibility reasons i think its a good idea to require alt text for every image
+  imageAltText: {type: String, required: false, minlength =1, maxlength = 100}
+
 });
 
 userSchema.plugin(uniqueValidator);
