b61b5858a38a919f186d59ec81e59a61c1aa0376
adimarogona <adimarogona@gmail.com>
2021-03-29 13:27:50 -0700

    added bed model

--- backend/models/bedModel.js
+++ backend/models/bedModel.js
@@ -6,8 +6,7 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
   date: { type: String, required: true },
   author: { type: mongoose.Types.ObjectId, required: true, ref: 'user' },
-  eventType: { type: mongoose.Types.ObjectId, required: true},
-  eventName: { type: String, minlength=1, maxlength=500}
+  numBeds: {type: int, required:true}
   
 
 });
