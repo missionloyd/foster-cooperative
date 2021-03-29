1232a37ba2bb575d1482bb8f6bb1048acbf0bab1
adimarogona <adimarogona@gmail.com>
2021-03-29 13:30:39 -0700

    changed calendar model

--- backend/models/calendarModel.js
+++ backend/models/calendarModel.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
-
+//calendar model here refers to each individual calendar event
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
   date: { type: String, required: true },
-  event: { type: String, minlength=1, maxlength=500}
+  author: { type: mongoose.Types.ObjectId, required: true, ref: 'user' },
+  numBeds: {type: int, required:true}
   
 
 });
